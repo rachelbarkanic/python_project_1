f75f1918e44faf0c3e96444c8291301881b5a58d
Rachel Barkanic <rachelbarkanic@Rachels-MacBook-Air.local>
2022-12-11 13:13:02 -0800

    started part 2

--- starter/part-2/part_2.py
+++ starter/part-2/part_2.py
@@ -1,37 +1,44 @@
 ### Step 1 - Lists ###
 
 # Fill in this list with several authors you are a fan of. At least 7 or 8 should do.
-my_authors = []
+my_authors = ['John Steinbeck', 'Delia Owens', 'Kristin Hannah', 'Lisa Wingate', 'Michael Crichton', 'Colleen Hoover', 'Kent Nerburn']
 
 # Now let's add a new author to the end with the .append() method. Type your code below.
 
-# Code here
+my_authors.append('Paulo Coelho')
 # Example: my_authors.append("H.G. Wells")
+
+
 
 
 # Now let's remove an element in the list
 
-# Code here
+my_authors.remove("Lisa Wingate")
 # Example: my_authors.remove("H.G. Wells")
+
+
 
 
 # Now access an element by it's index. (Remember it indexes start at 0.)
 
-# Code here
+my_authors[4]
 # Example: my_authors[2]
+
+
 
 
 # Now slice the list.
 
-# Code here
+my_authors[0:2]
 # Example: my_authors[1:4]
+
 
 
 ### Step 2 - Tuples ###
 
 # Create your tuple below. Assign it to a variable name you can reference later in the exercise.
 
-# Code here
+author_tuple = ('Delia Owens', 'Michael Crichton', 'John Steinbeck')
 # Example: my_author_tuple = ("F. Scott Fitzgerald", "J.K. Rowling", "Ernest Hemingway", "Emily Dickenson", "George Orwell", "Ray Bradbury")
 
 
@@ -39,19 +46,20 @@ my_authors = []
 
 # Create a set with your authors.
 
-# Code here
+author_set = {'John Steinbeck', 'Delia Owens', 'Kristin Hannah', 'Lisa Wingate', 'Michael Crichton', 'Colleen Hoover', 'Kent Nerburn', 'Paolo Coelho'}
 # Example: my_author_set = {"F. Scott Fitzgerald", "J.K. Rowling", "Ernest Hemingway", "Emily Dickenson", "George Orwell", "Ray Bradbury"}
 
 
 # Add a new author to your set.
 
-# Code here
+author_set.add('Jeanine Cummins')
 # Example: my_author_set.add("J.R.R. Tolkien")
 
 
 # Try adding the same author again, and be sure to print your set.
 
-# Code here
+author_set.add('Jeanine Cummins')
+print(author_set)
 # Example: my_author_set.add("J.R.R. Tolkien")
 
 
