c20e4e6e7c4c0578343c48138912bfa852ad3194
Your Name <rachelbarkanic@Rachels-MacBook-Air.local>
2022-12-20 10:06:00 -0800

    complete

--- starter/part-5/part_5.py
+++ starter/part-5/part_5.py
@@ -43,8 +43,9 @@ def book_list():
 
         for line in file:
             title, author, year, rating, pages = line.split(", ")
+            
 
-            print(f"Book Title: {title}, Author: {author}, Year: {year}, Rating: {rating}, Number of Pages: {pages}")
+            print(f"\nBook Title: {title}, Author: {author}, Year: {year}, Rating: {rating}, Number of Pages: {pages}")
 
 # book_list()
 
@@ -83,6 +84,28 @@ def book_list():
 
 ## Now follow the instructions in this final step. Expand your project. Clean up the code. Make your application functional. Great job getting your first Python application finished!
 
+books= []
+# made global to use in another func
+
+def book_dictionaries(library):
+
+    # books= []
+
+    with open('library.txt', "r") as f:
+        file = f.readlines()
+        for line in file:
+            title, author, year, rating, pages = line.split(", ")
+            books.append({
+                "title": title,
+                "author": author,
+                "year": int(year),
+                "rating": float(rating),
+                "pages": int(pages)
+            })
+    return books
+
+book_dictionaries('library.txt')
+
 
 def title_with_rating():
     with open("library.txt", "r") as f:
@@ -93,21 +116,43 @@ def title_with_rating():
 
             print(f"Book Title: {title}, Rating: {rating},")
 
+def total_pages(books):
+    pages = 0
+
+    for book in books:
+        pages += int(book["pages"])
+
+    return pages
+
+def books_over_rating(rating):
+#This function returns the titles of all books over the rating inputted by the user
+    rating_list = []
+
+    for book in books:
+        if int(book['rating']) >= rating:
+            rating_list.append(book['title'])
+    return rating_list
+
+print(books_over_rating(4.8))
+
 
 def main_menu(library):
     
     menu_run = True
     
     while menu_run:
-        opt = int(input("Enter 1 to add a new book,\n 2 to see all of the books on the shelf,\n 3 to see all of the book titles with their rating,\n 4 to sdfjhsadlf,\n 5 to sdfjh,\n or 6 for none of the above "))
+        opt = int(input("Enter:\n 1 to add a new book,\n 2 to see all of the books in the library,\n 3 to see all of the book titles with their rating,\n 4 to get the total number of pages in the library,\n 5 to get a list of book titles over the rating of your choice,\n or 6 for none of the above "))
         if opt == 1:
             new_book()
         elif opt == 2:
             book_list()
         elif opt == 3:
             title_with_rating()
-        # elif opt == 4:
-        # elif opt == 5:
+        elif opt == 4:
+            print(total_pages(books))
+        elif opt == 5:
+            rating = float(input('Enter a rating from 1.0 to 5.0 to see what books are rated at that level or higher! '))
+            print(books_over_rating(rating))
         elif opt == 6:
             print("Thanks, Bye")
             menu_run = False
